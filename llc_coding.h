99ee3d7c4b7830d27dce3d41c36aacfd73fd0c5b
vincentcheng62 <vincentcheng62@gmail.com>
2016-05-29 20:46:58 -0700

    Update llc_coding.h

--- llc_coding.h
+++ llc_coding.h
@@ -9,8 +9,8 @@ static void LLC_Coding(const cv::Mat &feavec,
 struct LLC_para
 {
 	unsigned int KNN_Build_Num_Of_Tree=4;// construct an randomized kd-tree index using 4 kd-trees
-    unsigned int KNN_traverse_time=16;	// larger means more accurate but costs more time
-    unsigned int knn_k=5;			// number of nearest neighbour to find, 5 is optimal according to paper
-    unsigned int Num_of_pyramid_level=3;	// 3 means [1x1, 2x2, 4x4]
+	unsigned int KNN_traverse_time=16;	// larger means more accurate but costs more time
+	unsigned int knn_k=5;			// number of nearest neighbour to find, 5 is optimal according to paper
+	unsigned int Num_of_pyramid_level=3;	// 3 means [1x1, 2x2, 4x4]
 };		
 #endif
